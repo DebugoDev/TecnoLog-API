7a4ec23a58d096ce763707c4159b3d18049fadd3
loregbrw <loregobara@gmail.com>
2025-11-26 10:20:05 -0300

    feat: local changes

---
+++ TecnoLog-API/Infrastructure/Migrations/20251126114740_UpdateRegister.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateRegister : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "stock_situation",
+                table: "tb_stock_item");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<short>(
+                name: "stock_situation",
+                table: "tb_stock_item",
+                type: "smallint",
+                nullable: true);
+        }
+    }
+}
